510510ca661842ea58c3e10c2c173204aac9e878
RishiAP <debjyotim2019@gmail.com>
2024-06-14 03:08:19 +0530

    Fixed types and build issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Navbar currentActive=""/>
       <div className="container">
         <TopNews/>
-        <MainWindow newsExist={false} articles={[]}/>
+        <MainWindow newsExist={false} news={{title:"",slug:"",content:{},topimage:"",metadesc:"",category:"",date:"",views:0,}} articles={[]} loading={false} showRescents={true}  />
       </div>
       <Footer/>
     </>
